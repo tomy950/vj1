e3c57d65758a8967405ca820005959460a563fae
tomy950 <mihaljevic242@gmail.com>
2020-05-06 15:08:03 +0200

    Dodana metoda

---
+++ vjezba1/NewClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba1
+{
+    class NewClass
+    {
+        static void MyMethod()
+        {
+            // code to be executed
+        }
+
+    }
+}
